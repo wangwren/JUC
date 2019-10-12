4535b48c8af0133e85b09bad7aaa25c6f4271fd4
wangwren <wangwren0906@gmail.com>
2019-10-12 18:38:10 +0800

    ThreadLocal

--- src/main/java/juc/c_027_ThreadLocal/ThreadLocal02.java
+++ src/main/java/juc/c_027_ThreadLocal/ThreadLocal02.java
@@ -1,10 +1,57 @@
 package juc.c_027_ThreadLocal;
 
 /**
+ * ThreadLocal线程局部变量
+ *
+ * ThreadLocal是使用空间换时间，synchronized是使用
+ *
+ * 比如在hibernate中session就存在与ThreadLocal中，避免synchronized的使用
+ *
+ * 运行下面的程序，理解ThreadLocal
+ *
  * @author: wangwren
- * @date: 2019/10/12
- * @description: ThreadLocal的使用
- * @version: 1.0
  */
 public class ThreadLocal02 {
+
+    //volatile static Person p = new Person();
+
+    /*volatile*/ static ThreadLocal<Person> tl = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("睡两秒的" + tl.get());
+        }).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            tl.set(new Person());
+
+            System.out.println("睡1秒的===" + tl.get().name);
+
+        }).start();
+
+        /**
+        * 运行后，观察控制台输出：
+        * 睡1秒的===zhangsan
+        * 睡两秒的null
+        *
+        * 两个线程使用同一个tl，但是睡1秒的线程创建了Person对象后，睡两秒的对象根本没有
+        * 说明两个线程有各自的想法
+        * 可以读一下ThreadLocal的set源码可以看出其中是维护了一个ThreadLocalMap
+        *
+        * https://www.cnblogs.com/dolphin0520/p/3920407.html
+        */
+    }
 }
